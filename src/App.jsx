f7931c9b9d5de3331671d6828a38332083024c78
Balaram Chandrabanshi <balaramchandrabanshi01@gmail.com>
2026-07-03 08:50:30 +0545

    Initial React project setup with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      Welcome to Movie Explorer
+    </div>
+  )
+}
+
+export default App
