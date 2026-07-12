febec4712ba91b18e65118f2565cacbb39b62729
Balaram Chandrabanshi <balaramchandrabanshi01@gmail.com>
2026-07-12 22:43:47 +0545

    feat: add loading state for popular movies

--- src/components/PopularMovies.jsx
+++ src/components/PopularMovies.jsx
@@ -4,12 +4,27 @@ import { getPopularMovies } from "../services/tmdb.js";
 
 const PopularMovies = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
+    
     getPopularMovies()
       .then((movies) => setMovies(movies))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setLoading(false);
+      })
+      
   }, []);
+
+  if (loading) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-2xl font-semibold">Loading...</p>
+    </div>
+  );
+}
 
   return (
     <div>
