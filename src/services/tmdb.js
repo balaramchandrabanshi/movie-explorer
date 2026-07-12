6ef0f124cb40a9523623afaa3ca728247a3d26a9
Balaram Chandrabanshi <balaramchandrabanshi01@gmail.com>
2026-07-12 22:11:52 +0545

    refactor: move TMDB API calls to service layer

---
+++ src/services/tmdb.js
@@ -0,0 +1,22 @@
+const accessToken = import.meta.env.VITE_TMDB_API_TOKEN;
+
+export function getPopularMovies() {
+  const url =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  return fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json.results)
+      return json.results;
+    })
+    .catch((err) => console.error(err));
+}
